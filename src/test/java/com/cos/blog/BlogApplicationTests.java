2d70546f7a36b2cff94a039207b6b9146dd05541
highspeeder <asdf3765@naver.com>
2023-09-27 18:30:35 +0900

    .

--- src/test/java/com/cos/blog/BlogApplicationTests.java
+++ src/test/java/com/cos/blog/BlogApplicationTests.java
@@ -2,20 +2,12 @@ package com.cos.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class BlogApplicationTests {
 
 	@Test
-	void 해쉬_암호화() {
-		// given
-		String encPassword = new BCryptPasswordEncoder().encode("1234");
-		
-		// when
-
-		// then
-		System.out.println("1234 해쉬:" + encPassword);
+	void contextLoads() {
 	}
 
 }
