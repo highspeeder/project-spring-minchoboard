8a1286a03e4865bdd4850f975a4e9c1de9d385cc
highspeeder <asdf3765@naver.com>
2023-09-30 00:15:36 +0900

    알람 메세지 삭제

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,147 +1,166 @@
 let index = {
-    init: function () {
-        $("#btn-save").on("click", () => { //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
-            this.save();
-        });
+  init: function () {
+    $("#btn-save").on("click", () => {
+      //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
+      this.save();
+    });
 
-        $("#btn-delete").on("click", () => { //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
-            this.deleteById();
-        });
+    $("#btn-delete").on("click", () => {
+      //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
+      this.deleteById();
+    });
 
-        $("#btn-update").on("click", () => { //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
-            this.update();
-        });
+    $("#btn-update").on("click", () => {
+      //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
+      this.update();
+    });
 
-        $("#btn-reply-save").on("click", () => { //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
-            this.replySave();
-        });
-    },
+    $("#btn-reply-save").on("click", () => {
+      //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
+      this.replySave();
+    });
+  },
 
-    save: function () {
-        let data = {
-            title: $("#title").val(),
-            content: $("#content").val(),
-        }
+  save: function () {
+    let data = {
+      title: $("#title").val(),
+      content: $("#content").val(),
+    };
 
-        var token = $("meta[name='_csrf']").attr("content");
-        var header = $("meta[name='_csrf_header']").attr("content");
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
 
-        //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
-        $.ajax({
-            type: "POST",
-            url: "/api/board",
-            data: JSON.stringify(data), //http body
-            contentType: "application/json; charest=utf-8", //MIME타입
-            beforeSend: function (xhr) {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
-                xhr.setRequestHeader(header, token);
-            },
-        }).fail(function (error) {
-            alert("글쓰기가 실패 하였습니다.");
-            alert(error.responseText);
-        }).done(function (resp) {
-            alert("글쓰기가 완료 되었습니다.");
-            location.href = "/";
-        })
-    },
+    //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+    $.ajax({
+      type: "POST",
+      url: "/api/board",
+      data: JSON.stringify(data), //http body
+      contentType: "application/json; charest=utf-8", //MIME타입
+      beforeSend: function (xhr) {
+        /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+        xhr.setRequestHeader(header, token);
+      },
+    })
+      .fail(function (error) {
+        alert("글쓰기가 실패 하였습니다.");
+        //alert(error.responseText);
+      })
+      .done(function (resp) {
+        alert("글쓰기가 완료 되었습니다.");
+        location.href = "/";
+      });
+  },
 
-    deleteById: function () {
-        let id = $("#id").text();
+  deleteById: function () {
+    let id = $("#id").text();
 
-        var token = $("meta[name='_csrf']").attr("content");
-        var header = $("meta[name='_csrf_header']").attr("content");
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
 
-        //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
-        $.ajax({
-            type: "DELETE",
-            url: "/api/board/" + id,
-            dataType: "json",
-            contentType: "application/json; charest=utf-8", //MIME타입
-            beforeSend: function (xhr) {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
-                xhr.setRequestHeader(header, token);
-            },
-        }).done(function (resp) {
-            alert("글 삭제가 완료 되었습니다.");
-            location.href = "/";
-        }).fail(function (error) {
-            alert("글 삭제가 실패 하였습니다.");
-            alert(error.responseText);
-        })
-    },
+    //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+    $.ajax({
+      type: "DELETE",
+      url: "/api/board/" + id,
+      dataType: "json",
+      contentType: "application/json; charest=utf-8", //MIME타입
+      beforeSend: function (xhr) {
+        /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+        xhr.setRequestHeader(header, token);
+      },
+    })
+      .done(function (resp) {
+        alert("글 삭제가 완료 되었습니다.");
+        location.href = "/";
+      })
+      .fail(function (error) {
+        alert("글 삭제가 실패 하였습니다.");
+        //alert(error.responseText);
+      });
+  },
 
-    update: function () {
-        let id = $("#id").val();
+  update: function () {
+    let id = $("#id").val();
 
-        let data = {
-            title: $("#title").val(),
-            content: $("#content").val(),
-        }
+    let data = {
+      title: $("#title").val(),
+      content: $("#content").val(),
+    };
 
-        var token = $("meta[name='_csrf']").attr("content");
-        var header = $("meta[name='_csrf_header']").attr("content");
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
 
-        //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
-        $.ajax({
-            type: "PUT",
-            url: "/api/board/"+id,
-            data: JSON.stringify(data), //http body
-            contentType: "application/json; charest=utf-8", //MIME타입
-            beforeSend: function (xhr) {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
-                xhr.setRequestHeader(header, token);
-            },
-        }).done(function (resp) {
-            alert("글수정이 완료 되었습니다.");
-            location.href = "/";
-        }).fail(function (error) {
-            alert("글수정이 실패 하였습니다.");
-            alert(error.responseText);
-        })
-    },
+    //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+    $.ajax({
+      type: "PUT",
+      url: "/api/board/" + id,
+      data: JSON.stringify(data), //http body
+      contentType: "application/json; charest=utf-8", //MIME타입
+      beforeSend: function (xhr) {
+        /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+        xhr.setRequestHeader(header, token);
+      },
+    })
+      .done(function (resp) {
+        alert("글수정이 완료 되었습니다.");
+        location.href = "/";
+      })
+      .fail(function (error) {
+        alert("글수정이 실패 하였습니다.");
+        //alert(error.responseText);
+      });
+  },
 
-    replySave: function () {
-        let data = {
-            userId: $("#userId").val(),
-            boardId: $("#boardId").val(),
-            content: $("#reply-content").val(),
-        }
+  replySave: function () {
+    let data = {
+      userId: $("#userId").val(),
+      boardId: $("#boardId").val(),
+      content: $("#reply-content").val(),
+    };
 
-        var token = $("meta[name='_csrf']").attr("content");
-        var header = $("meta[name='_csrf_header']").attr("content");
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
 
-        $.ajax({
-            type: "POST",
-            url: `/api/board/${data.boardId}/reply`, // `는 작은 따옴표(')가 아니고 백틱(`)임 변수를 글자안에 넣기 위함.
-            data: JSON.stringify(data), //http body
-            contentType: "application/json; charest=utf-8", //MIME타입
-            beforeSend: function (xhr) {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
-                xhr.setRequestHeader(header, token);
-            },
-        }).fail(function (error) {
-            alert("댓글쓰기가 실패 하였습니다.");
-            alert(error.responseText);
-        }).done(function (resp) {
-            alert("댓글쓰기가 완료 되었습니다.");
-            location.href = `/board/${data.boardId}`;
-        })
-    },
+    $.ajax({
+      type: "POST",
+      url: `/api/board/${data.boardId}/reply`, // `는 작은 따옴표(')가 아니고 백틱(`)임 변수를 글자안에 넣기 위함.
+      data: JSON.stringify(data), //http body
+      contentType: "application/json; charest=utf-8", //MIME타입
+      beforeSend: function (xhr) {
+        /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+        xhr.setRequestHeader(header, token);
+      },
+    })
+      .fail(function (error) {
+        alert("댓글쓰기가 실패 하였습니다.");
+        //alert(error.responseText);
+      })
+      .done(function (resp) {
+        alert("댓글쓰기가 완료 되었습니다.");
+        location.href = `/board/${data.boardId}`;
+      });
+  },
 
-    replyDelete: function (boardId, replyId) {
-        var token = $("meta[name='_csrf']").attr("content");
-        var header = $("meta[name='_csrf_header']").attr("content");
+  replyDelete: function (boardId, replyId) {
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
 
-        $.ajax({
-            type: "DELETE",
-            url: `/api/board/${boardId}/reply/${replyId}`, // `는 작은 따옴표(')가 아니고 백틱(`)임 변수를 글자안에 넣기 위함.
-            beforeSend: function (xhr) {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
-                xhr.setRequestHeader(header, token);
-            },
-        }).done(function (resp) {
-            alert("댓글삭제가 성공 하였습니다.");
-            location.href = `/board/${boardId}`;
-        }).fail(function (error) {
-            alert("댓글삭제가 실패 하였습니다.");
-            alert(error.responseText);
-        })
-    },
-}
+    $.ajax({
+      type: "DELETE",
+      url: `/api/board/${boardId}/reply/${replyId}`, // `는 작은 따옴표(')가 아니고 백틱(`)임 변수를 글자안에 넣기 위함.
+      beforeSend: function (xhr) {
+        /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+        xhr.setRequestHeader(header, token);
+      },
+    })
+      .done(function (resp) {
+        alert("댓글삭제가 성공 하였습니다.");
+        location.href = `/board/${boardId}`;
+      })
+      .fail(function (error) {
+        alert("댓글삭제가 실패 하였습니다.");
+        //alert(error.responseText);
+      });
+  },
+};
 
 index.init();
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,106 +1,115 @@
 let index = {
-    init:function () {
-        $("#btn-save").on("click", ()=>{ //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
-            this.save();
-        });
+  init: function () {
+    $("#btn-save").on("click", () => {
+      //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
+      this.save();
+    });
 
-        $("#btn-update").on("click", ()=>{ //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
-            this.update();
-        });
+    $("#btn-update").on("click", () => {
+      //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
+      this.update();
+    });
 
-        $("#btn-login").on("click", ()=>{ //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
-            this.login();
-        });
-    },
+    $("#btn-login").on("click", () => {
+      //function대신 ()=>를 사용하는 이유는 this를 바인딩하기 위해
+      this.login();
+    });
+  },
 
-    save: function () {
-        let data = {
-            username: $("#username").val(),
-            password: $("#password").val(),
-            email: $("#email").val(),
-        }
-        
-        var token = $("meta[name='_csrf']").attr("content");
-		var header = $("meta[name='_csrf_header']").attr("content");
+  save: function () {
+    let data = {
+      username: $("#username").val(),
+      password: $("#password").val(),
+      email: $("#email").val(),
+    };
 
-        //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
-        $.ajax({
-            type: "POST",
-            url: "/auth/joinProc",
-            data: JSON.stringify(data), //http body
-            contentType: "application/json; charest=utf-8", //MIME타입
-            beforeSend : function(xhr)
-            {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
-				xhr.setRequestHeader(header, token);
-            },
-        }).done(function (resp) {
-            alert("회원가입이 완료 되었습니다.");
-            location.href = "/";
-        }).fail(function (error) {
-            alert("회원가입을 실패하였습니다.");
-            alert(error.responseText);
-        })
-    },
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
 
-    update: function () {
-        let data = {
-            id: $("#id").val(),
-            username: $("#username").val(),
-            password: $("#password").val(),
-            email: $("#email").val(),
-        }
-        
-        var token = $("meta[name='_csrf']").attr("content");
-		var header = $("meta[name='_csrf_header']").attr("content");
+    //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+    $.ajax({
+      type: "POST",
+      url: "/auth/joinProc",
+      data: JSON.stringify(data), //http body
+      contentType: "application/json; charest=utf-8", //MIME타입
+      beforeSend: function (xhr) {
+        /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+        xhr.setRequestHeader(header, token);
+      },
+    })
+      .done(function (resp) {
+        alert("회원가입이 완료 되었습니다.");
+        location.href = "/";
+      })
+      .fail(function (error) {
+        alert("회원가입을 실패하였습니다.");
+        //alert(error.responseText);
+      });
+  },
 
-        //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
-        $.ajax({
-            type: "PUT",
-            url: "/user",
-            data: JSON.stringify(data), //http body
-            contentType: "application/json; charest=utf-8", //MIME타입
-            beforeSend : function(xhr)
-            {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
-				xhr.setRequestHeader(header, token);
-            },
-        }).done(function (resp) {
-            alert("수정이 완료 되었습니다.");
-            location.href = "/";
-        }).fail(function (error) {
-            alert("수정이 실패하였습니다.");
-            alert(error.responseText);
-        })
-    },
+  update: function () {
+    let data = {
+      id: $("#id").val(),
+      username: $("#username").val(),
+      password: $("#password").val(),
+      email: $("#email").val(),
+    };
 
-    login: function () {
-        let data = {
-            username: $("#username").val(),
-            password: $("#password").val(),
-        }
-        
-        var token = $("meta[name='_csrf']").attr("content");
-		var header = $("meta[name='_csrf_header']").attr("content");
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
 
-        var requestBody = $.param(data); // 데이터 직렬화
-        
-        $.ajax({
-            type: "POST",
-            url: "/auth/loginProc",
-            data: requestBody,
-            // data: JSON.stringify(data), //json으로 변경
-            contentType: "application/x-www-form-urlencoded; charset=utf-8", //MIME타입
-            beforeSend : function(xhr)
-            {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
-				xhr.setRequestHeader(header, token);
-            },
-        }).done(function (resp) {
-            alert("로그인이 완료 되었습니다.");
-            location.href = "/";
-        }).fail(function (error) {
-            alert("로그인을 실패하였습니다.");
-            alert(error.responseText);
-        })
-    },
-}
+    //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+    $.ajax({
+      type: "PUT",
+      url: "/user",
+      data: JSON.stringify(data), //http body
+      contentType: "application/json; charest=utf-8", //MIME타입
+      beforeSend: function (xhr) {
+        /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+        xhr.setRequestHeader(header, token);
+      },
+    })
+      .done(function (resp) {
+        alert("수정이 완료 되었습니다.");
+        location.href = "/";
+      })
+      .fail(function (error) {
+        alert("수정이 실패하였습니다.");
+        //alert(error.responseText);
+      });
+  },
+
+  login: function () {
+    let data = {
+      username: $("#username").val(),
+      password: $("#password").val(),
+    };
+
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+
+    var requestBody = $.param(data); // 데이터 직렬화
+
+    $.ajax({
+      type: "POST",
+      url: "/auth/loginProc",
+      data: requestBody,
+      // data: JSON.stringify(data), //json으로 변경
+      contentType: "application/x-www-form-urlencoded; charset=utf-8", //MIME타입
+      beforeSend: function (xhr) {
+        /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+        xhr.setRequestHeader(header, token);
+      },
+    })
+      .done(function (resp) {
+        alert("로그인이 완료 되었습니다.");
+        location.href = "/";
+      })
+      .fail(function (error) {
+        alert("로그인을 실패하였습니다.");
+        //alert(error.responseText);
+      });
+  },
+};
 
 index.init();
